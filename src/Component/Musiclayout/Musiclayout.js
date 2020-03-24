0d5671de2e4baf7bb71097f0a49b99d9f08fe51c
omkarpai <51873597+omkarpai@users.noreply.github.com>
2020-03-24 13:50:14 +0530

    removed white bar since progress bar not used

--- src/Component/Musiclayout/Musiclayout.js
+++ src/Component/Musiclayout/Musiclayout.js
@@ -11,7 +11,7 @@ let Musicplayer = (props) => {
         <img className={classes.image} src={props.image} alt="no image available"></img>
         <div className={classes.artistname}>{props.artist}</div>
         <div className={classes.songname}>{props.songname}</div>
-        <div className={classes.bar}></div>
+        {/* <div className={classes.bar}></div> */}
         <input className={classes.like}  type="image" src={like} onClick={props.likeb}></input>
         <input className={classes.cross} type="image" src={cross} onClick={props.dislikeb}></input>
         <input className={classes.play} type="image" src={play} onClick={props.playb} ></input>
