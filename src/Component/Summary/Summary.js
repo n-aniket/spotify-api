c0e06f69b6689d4ca35c645f3e5b5e953ad09951
omkarpai <51873597+omkarpai@users.noreply.github.com>
2020-03-30 18:53:36 +0530

    restructuring

--- src/Component/Summary/Summary.js
+++ src/Component/Summary/Summary.js
@@ -4,10 +4,11 @@ import Aux from '../../hoc/Auxilary';
 
 const summary = (props) => (
     <Aux>
-        <div className={classes.bg}></div> 
-        <div className={classes.Summ}>SUMMARY
-            <div className={classes.desc}>{props.description}</div>
-        </div>
+        <div className={classes.bg}>
+            <div className={classes.Summ}>SUMMARY
+                <div className={classes.desc}>{props.description}</div>
+            </div>
+        </div> 
         
     </Aux>
         
